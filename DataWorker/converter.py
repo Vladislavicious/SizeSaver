c6e5e223aa089a0fa31dd10d500b0d778693e370
Vladislavicious <vladshilov03@mail.ru>
2024-08-06 19:16:08 +0300

    Добавил тесты для dataConverter'а

--- DataWorker/converter.py
+++ DataWorker/converter.py
@@ -1,3 +1,53 @@
+from datetime import datetime
+import pandas as pd
+from typing import Tuple, List
+
+from Misc.codes import ResultCodes
+
+def get_correct_headers() -> List[str]:
+  return ["text", "data", "bss", "dec", "hex", "filename"]
+
+def get_refined_headers() -> List[str]:
+  return ["code(text)", "constants(data)", "variables(bss)",
+          "full(dec)", "last modified", "filename"]
+
+def reduce_ints_to_kb(value):
+  if type(value) is not str:
+    return value / 1024
+  else:
+    return value
+
 class DataConverter:
   def __init__(self) -> None:
-    pass
+    self.__df = pd.DataFrame()
+
+  def Initialize(self, df: pd.DataFrame, date: datetime) -> Tuple[ResultCodes, str]:
+    self.__df = df.copy(True)
+    self.__date = date
+    return (ResultCodes.OK, "All ok")
+
+  def Refine(self) -> Tuple[ResultCodes, str]:
+    if pd.DataFrame().equals(self.__df):
+      return (ResultCodes.BAD_DATA, "Bad data given")
+
+    headers = pd.Index(get_correct_headers())
+    given_headers = self.__df.columns
+    if given_headers.equals(headers) is True:
+      return (ResultCodes.OK, "All ok")
+
+    return (ResultCodes.BAD_FILE_FORMAT, f"Bad Header: {given_headers}")
+
+  def Reshape(self) -> pd.DataFrame:
+    newNames = dict(zip(get_correct_headers(), get_refined_headers()))
+    self.__df.rename(columns=newNames, inplace=True)
+
+    self.__df = self.__df.map(reduce_ints_to_kb)
+    self.__df['last modified'] = pd.Series(self.__df['last modified'], dtype="datetime64[m]")
+
+    data = pd.to_datetime( self.__date )
+
+    self.__df.loc[0, 'last modified'] = data
+    return self.__df
+
+  def getData(self) -> pd.DataFrame:
+    return self.__df
--- Tests/data_converter_test.py
+++ Tests/data_converter_test.py
@@ -1,10 +1,62 @@
 import unittest
-
+from datetime import datetime
 from Misc.codes import ResultCodes
-from DataWorker.converter import DataConverter
+from DataWorker.converter import DataConverter, get_correct_headers
 
+import pandas as pd
 
 class TestDataConverter(unittest.TestCase):
+  def common_BaseInit(self, df: pd.DataFrame) -> DataConverter:
+    saver = DataConverter()
+    date = datetime.today()
+    saver.Initialize(df, date)
+    return saver
+
   def test_InitializeDataConverter(self):
     saver = DataConverter()
     self.assertEqual(type(saver), DataConverter)
+
+  def test_TakesCorrectData(self):
+    df = pd.DataFrame()
+    saver = self.common_BaseInit(df)
+    result = df.equals(saver.getData())
+    self.assertTrue(result)
+
+  def test_RefineNoDf(self):
+    saver = self.common_BaseInit(pd.DataFrame())
+    result, description = saver.Refine()
+    self.assertEqual(result, ResultCodes.BAD_DATA, description)
+
+  def test_RefineGoodDf(self):
+    headers = get_correct_headers()
+    values = [[1 * 1024], [2 * 1024], [3 * 1024], [4 * 1024], [5 * 1024], ["some/path/to/file"]]
+    data = dict(zip(headers, values))
+    df = pd.DataFrame.from_dict(data)
+
+    saver = self.common_BaseInit(df)
+    result, description = saver.Refine()
+    self.assertEqual(result, ResultCodes.OK, description)
+
+  def test_RefineBadDf(self):
+    headers = ["a", "b", "c", "d", "e", "f"]
+    values = [[1 * 1024], [2 * 1024], [3 * 1024], [4 * 1024], [5 * 1024], ["some/path/to/file"]]
+    data = dict(zip(headers, values))
+    df = pd.DataFrame.from_dict(data)
+
+    saver = self.common_BaseInit(df)
+
+    result, description = saver.Refine()
+    self.assertEqual(result, ResultCodes.BAD_FILE_FORMAT, description)
+
+  def test_ReshapeGoodDf(self):
+    headers = get_correct_headers()
+    values = [[1 * 1024], [2 * 1024], [3 * 1024], [4 * 1024], [5 * 1024], ["some/path/to/file"]]
+    data = dict(zip(headers, values))
+    df = pd.DataFrame.from_dict(data)
+
+    saver = self.common_BaseInit(df)
+    result, description = saver.Refine()
+    self.assertEqual(result, ResultCodes.OK, description)
+
+    newDf = saver.Reshape()
+    self.assertEqual(type(newDf), pd.DataFrame)
